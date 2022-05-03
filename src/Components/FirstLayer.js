275744c279eea98346a2cf5dd6a5346d9f374f14
imevanc <charal.vag@gmail.com>
2022-05-03 14:59:46 +0100

    responsive toggles

--- src/Components/FirstLayer.js
+++ src/Components/FirstLayer.js
@@ -1,32 +1,27 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
-import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import ToggleGroup from "./ToggleGroup";
 
 const FirstLayer = () => {
   return (
     <Container
-      component="main"
       sx={{
         display: "flex",
-        justifyContent: "flex-end",
-        minWidth: "320px",
+        flexDirection: "column",
+        justifyContent: "center",
+        background: "linear-gradient(180deg, #F6B868 0%, #EE6B2D 100%)",
+        maxWidth: "1280px",
+        maxHeight: "720px",
       }}
     >
-      <Paper
-        sx={{
-          position: "relative",
-          width: "1280px",
-          height: "720px",
-          background: "linear-gradient(180deg, #F6B868 0%, #EE6B2D 100%)",
-        }}
-      >
-        <Typography component="h1" variant="h4" align="center">
-          Question
-        </Typography>
-        <ToggleGroup />
-      </Paper>
+      <Typography component="h1" variant="h4" align="center">
+        Question
+      </Typography>
+      <ToggleGroup />
+      <ToggleGroup />
+      <ToggleGroup />
+      <ToggleGroup />
     </Container>
   );
 };
--- src/Components/ToggleGroup.js
+++ src/Components/ToggleGroup.js
@@ -1,31 +1,90 @@
 import * as React from "react";
+import Box from "@mui/material/Box";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Typography from "@mui/material/Typography";
 
 const ToggleGroup = () => {
-  const [alignment, setAlignment] = React.useState("web");
+  const matches = useMediaQuery("(min-width:600px)");
+  const [alignment, setAlignment] = React.useState("left");
 
-  const handleChange = (_, newAlignment) => {
+  const handleAlignment = (event, newAlignment) => {
     setAlignment(newAlignment);
   };
-
   return (
-    <ToggleButtonGroup
-      value={alignment}
-      exclusive
-      onChange={handleChange}
+    <Box
+      paddingTop={"20px"}
       sx={{
-        position: "absolute",
-        width: "900px",
-        height: "80px",
-        left: "190px",
-        top: "179px",
-        filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+        display: "flex",
+        justifyContent: "center",
       }}
     >
-      <ToggleButton value="web">Web</ToggleButton>
-      <ToggleButton value="ios">IOS</ToggleButton>
-    </ToggleButtonGroup>
+      <ToggleButtonGroup
+        value={alignment}
+        exclusive
+        onChange={handleAlignment}
+        orientation={`${matches ? `horizontal` : `vertical`}`}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          border: "2px solid #F9D29F",
+          borderRadius: "100px",
+          filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+        }}
+      >
+        <ToggleButton
+          value="left"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "21px 157px",
+            background: "#F8CAA3",
+            borderRadius: "100px",
+          }}
+        >
+          <Typography
+            sx={{
+              fontFamily: "Mulish",
+              fontSize: "24px",
+              fontStyle: "normal",
+              fontWeight: 700,
+              fontColor: "#9F938B",
+              textTransform: "none",
+            }}
+          >
+            left
+          </Typography>
+        </ToggleButton>
+        <ToggleButton
+          value="center"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "21px 157px",
+            background: "#F8CAA3",
+            borderRadius: "100px",
+          }}
+        >
+          <Typography
+            sx={{
+              fontFamily: "Mulish",
+              fontSize: "24px",
+              fontStyle: "normal",
+              fontWeight: 700,
+              fontColor: "#9F938B",
+              textTransform: "none",
+            }}
+          >
+            right
+          </Typography>
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </Box>
   );
 };
 
