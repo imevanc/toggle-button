9e4aa8c74335642d40d9f90ca85fb759211c31f4
imevanc <charal.vag@gmail.com>
2022-05-04 15:42:18 +0100

    aligned the toggles both on web and mobile approach

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LinearColor from "./Components/LinearColor";
 const App = () => {
   return questionsAndAnswers.questionsAndAnswers.length ? (
     <QAndAWidget
-      questionsAndAnswers={questionsAndAnswers.questionsAndAnswers[1]}
+      questionsAndAnswers={questionsAndAnswers.questionsAndAnswers[0]}
     />
   ) : (
     <LinearColor />
