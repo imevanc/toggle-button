3e4352df587ce4b94462865093982a1934da3076
imevanc <charal.vag@gmail.com>
2022-05-05 20:27:34 +0100

    alphabetical sorted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import QAndAWidget from "./Components/QAndAWidget";
-import questionsAndAnswers from "./db/db.json";
-import LinearColor from "./Components/LinearColor";
-import Theme from "./Context/TrafficLightTheme";
 import { ThemeContext } from "./Context/ThemeContext";
 import { ThemeProvider } from "@mui/material/styles";
+import LinearColor from "./Components/LinearColor";
+import QAndAWidget from "./Components/QAndAWidget";
+import questionsAndAnswers from "./db/db.json";
+import Theme from "./Context/TrafficLightTheme";
 
 const App = () => {
   const ourTheme = Theme();
--- src/Components/QAndAWidget.js
+++ src/Components/QAndAWidget.js
@@ -1,9 +1,9 @@
-import * as React from "react";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
 import { ThemeContext } from "../Context/ThemeContext";
-import Typography from "@mui/material/Typography";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import CustomToggleButton from "./CustomToggleButton";
+import Typography from "@mui/material/Typography";
 
 const QAndAWidget = (props) => {
   const ourTheme = React.useContext(ThemeContext);
